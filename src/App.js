ffdb1e29ba0559077130e2e7c2c22f0f843ef555
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-17 20:37:17 +0530

    counter and input field value example to see the use of useState Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+// example to understand the use of useState Hook 
+// form input can also possible  ??
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState('');
+
+  function Increment() {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  function valueTyped(e) {
+    setValue(e.target.value);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <p>Value of Count : {count}</p>
+      <button onClick={Increment} >Click to Increment </button>
+      <br />
+      <button onClick={decrement}>Click to decrease the value</button>
+      <p>-----------------------------------------------------------------------------</p>
+      <h2>The value which user typed is : {value}</h2>
+      <input type="text" onChange={valueTyped} />
     </div>
   );
 }
