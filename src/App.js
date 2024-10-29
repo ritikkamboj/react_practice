d3b098ff6b8018c02eac219e6df42b9e69ee5378
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-10-29 11:24:33 +0530

    basic example to see the use of useContext Hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-// example to understand the use of useState Hook 
-// form input can also possible  ??
+const valueContext = createContext();
+
 function App() {
 
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState('');
-
-  function Increment() {
-    setCount(count + 1)
-  }
-
-  const decrement = () => {
-    setCount(count - 1)
-  }
-
-  function valueTyped(e) {
-    setValue(e.target.value);
-
-  }
+  const [value, setValue] = useState(1);
   return (
+    <valueContext.Provider value={{ value }}>
+      <div>
 
-    <div>
-      <p>Value of Count : {count}</p>
-      <button onClick={Increment} >Click to Increment </button>
-      <br />
-      <button onClick={decrement}>Click to decrease the value</button>
-      <p>-----------------------------------------------------------------------------</p>
-      <h2>The value which user typed is : {value}</h2>
-      <input type="text" onChange={valueTyped} />
-    </div>
+        <Comp />
+      </div>
+    </valueContext.Provider>
   );
 }
+
+const Comp = () => {
+  const { value } = useContext(valueContext)
+  return <p> Jai maata di {value}</p>;
+};
 
 export default App;
