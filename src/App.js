73e026ced6665b23bd839c79a3f7260764097e2b
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-11-01 09:22:53 +0530

    redux code when we have only one reducer function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-// App.js
-import React from 'react';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import store from './store';
+import React from 'react'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import store from './store'
 
 function App() {
   const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.count);
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const count = useSelector((state) => state);
+
 
   return (
-    <div className="App">
-      <h1>Redux Example</h1>
-      <div>
-        <h2>Count: {count}</h2>
-        <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-        <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
-      </div>
-      <div>
-        <h2>Authentication: {isAuthenticated ? 'Logged In' : 'Logged Out'}</h2>
-        <button onClick={() => dispatch({ type: 'LOGIN' })}>Log In</button>
-        <button onClick={() => dispatch({ type: 'LOGOUT' })}>Log Out</button>
-      </div>
+    <div>
+      <p>Lets see it works or not </p>
+      <p> value of count is : {count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment </button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement </button>
     </div>
-  );
+  )
 }
 
-export default function RootApp() {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+function RootApp() {
+  return <Provider store={store}>
+    <App />
+  </Provider>
 }
+
+export default RootApp;
--- src/store.js
+++ src/store.js
@@ -1,41 +1,21 @@
-// store.js
-import { createStore, combineReducers } from 'redux';
+import { createStore } from "redux";
 
-// Initial States
-const initialCounterState = { count: 0 };
-const initialAuthState = { isAuthenticated: false };
+//defining initial state 
+const initialCounter = 0;
 
-// Reducer 1: Counter Reducer
-function counterReducer(state = initialCounterState, action) {
+function countReducer(state = initialCounter, action) {
     switch (action.type) {
-        case 'INCREMENT':
-            return { ...state, count: state.count + 1 };
-        case 'DECREMENT':
-            return { ...state, count: state.count - 1 };
+        case ("increment"):
+            return state + 1;
+
+        case ("decrement"):
+            return state - 1;
+
         default:
             return state;
     }
 }
 
-// Reducer 2: Authentication Reducer
-function authReducer(state = initialAuthState, action) {
-    switch (action.type) {
-        case 'LOGIN':
-            return { ...state, isAuthenticated: true };
-        case 'LOGOUT':
-            return { ...state, isAuthenticated: false };
-        default:
-            return state;
-    }
-}
-
-// Combine Reducers
-const rootReducer = combineReducers({
-    counter: counterReducer,
-    auth: authReducer,
-});
-
-// Create Store
-const store = createStore(rootReducer);
+const store = createStore(countReducer);
 
 export default store;
