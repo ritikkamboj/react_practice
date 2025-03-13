f10f2b76bb269217d0eb65911db215c0ff5bf325
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-13 17:59:14 +0530

    redux approach , previous commit is Redux toolkit approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  increment,
-  decrement,
-  multiply,
-  incrementByAmount,
-} from "./redux/counter/CounterSlice";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
+  const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
   return (
     <div>
-      <Navbar />
       <div>
-        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch({ type: "decre" })}>-</button>
         <p>The Value of count is {count}</p>
-        <button onClick={() => dispatch(increment())}>+</button>
-        <button onClick={() => dispatch(multiply())}>*</button>
+        <button onClick={() => dispatch({ type: "incre" })}>+</button>
+        {/* <button onClick={() => dispatch(multiply())}>*</button>
         <p>
           Click the below button if you want to incrase the valuw with some
           specific value{" "}
         </p>
-        <button onClick={() => dispatch(incrementByAmount(5))}>Surprise</button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>Surprise</button> */}
       </div>
     </div>
   );
