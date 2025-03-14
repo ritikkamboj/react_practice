36ceebb16efb61de5ca092edc6fb857c0acf5d62
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-14 23:05:50 +0530

    slice and splice use

--- src/practice.js
+++ src/practice.js
@@ -5,3 +5,18 @@
 const x = [1, 2, 3, 4, 5];
 const y = x.slice(1, 2);
 console.log(x, y);
+
+
+// splice 
+
+const numbers = [12, 24, 56, 76, 9, 8];
+
+let num = numbers.splice(0, 2);
+console.log(num);
+console.log(numbers); // [ 56, 76, 9, 8 ]
+
+const num2 = numbers.splice(2, 1, 69, 96);
+console.log(num2, numbers);
+
+
+
