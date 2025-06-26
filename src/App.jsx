4af373082705c0ea29558aada06fbd3aba4221e0
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-26 18:57:55 +0530

    see the use of useReducer hook for counter value

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,38 @@
-// another code practoce for useContext 
-
-import { createContext, useContext, useState } from "react";
-
-
-const OurContext = createContext();
+import React, { useReducer } from 'react'
+// use Reducer hook 
 
 
-function Provider({ children }) {
-  const [theme, setTheme] = useState('light');
-  const [name, setName] = useState('John Dow')
 
-  function toggleTheme() {
-    setTheme((prev) => prev === 'light' ? "dark" : "light")
+const initialState = {count : 0}
+
+function reducer(state , action)
+{
+  switch(action.type){
+    case "inc":
+      return {count : state.count + 1}
+    case 'dec':
+      return {count : state.count - 1}
+    case 'reset':
+      return {count : 0}
+    default :
+    throw new Error("mistake kardi ")
   }
 
-  function changeName() {
-
-    setName((prev)=> prev==='John Dow' ? "sydny sweeny" : "John Dow")
-  }
-
-
-  return <OurContext.Provider value={{ theme, toggleTheme, name, changeName }}>
-
-    {children}
-  </OurContext.Provider>
 }
 
-function Profile() {
-
-  const { theme, toggleTheme, name, changeName } = useContext(OurContext)
-
-
-  return <>
-    <h1>The value of theme is : {theme}</h1>
-    <h1>The value of name is : {name}</h1>
-    <button onClick={toggleTheme}>Chnage Theme</button>
-    <button onClick={changeName}>Change Name</button>
-
-  </>
-}
-
-import React from 'react'
 
 function App() {
-  return (
-    <Provider>
-      <Profile />
 
-    </Provider>
+  const [state, dispatch] =useReducer(reducer, initialState)
+  return (
+    <div>
+      <p>count value is : {state.count}</p>
+      <div>
+        <button onClick={()=> dispatch({type : "inc"})}>+</button>
+        <button onClick={()=> dispatch({type : "dec"})}>-</button>
+        <button onClick={()=> dispatch({type : "reset"})}>RESET</button>
+      </div>
+    </div>
   )
 }
 
