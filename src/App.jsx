5cc034bbce5e0ab522e82b2cdd49100fecb1d175
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-06-26 16:57:54 +0530

    context use to chnage the value of theme and nam e

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import React, { createContext, useContext, useState } from 'react'
-import ThemeValue from './components/ThemeValue';
-import ToggleTheme from './components/ToggleTheme';
-import Provider from './components/Context';
-// here we practice useContext hook , appliation as theme usage in the prgram 
+// another code practoce for useContext 
+
+import { createContext, useContext, useState } from "react";
 
 
+const OurContext = createContext();
 
+
+function Provider({ children }) {
+  const [theme, setTheme] = useState('light');
+  const [name, setName] = useState('John Dow')
+
+  function toggleTheme() {
+    setTheme((prev) => prev === 'light' ? "dark" : "light")
+  }
+
+  function changeName() {
+
+    setName((prev)=> prev==='John Dow' ? "sydny sweeny" : "John Dow")
+  }
+
+
+  return <OurContext.Provider value={{ theme, toggleTheme, name, changeName }}>
+
+    {children}
+  </OurContext.Provider>
+}
+
+function Profile() {
+
+  const { theme, toggleTheme, name, changeName } = useContext(OurContext)
+
+
+  return <>
+    <h1>The value of theme is : {theme}</h1>
+    <h1>The value of name is : {name}</h1>
+    <button onClick={toggleTheme}>Chnage Theme</button>
+    <button onClick={changeName}>Change Name</button>
+
+  </>
+}
+
+import React from 'react'
 
 function App() {
   return (
     <Provider>
-      <ThemeValue />
-      <ToggleTheme />
-
+      <Profile />
 
     </Provider>
   )
